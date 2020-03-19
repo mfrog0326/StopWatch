29b226647a9bb5b86dde982f153e85b76f6d6f99
mFrog <kcs326g1c2@gmail.com>
2020-03-19 19:59:24 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,14 +7,17 @@
   const reset = document.getElementById('reset');
 
 let startTime;
+let timeoutId;
+let elapsedTime = 0;
 
 function countUp() {
-  const d = new Date(Date.now() - startTime);
+  const d = new Date(Date.now() - startTime + elapsedTime);
   const m = String(d.getMinutes()).padStart(2, '0');
   const s = String(d.getSeconds()).padStart(2, '0');
   const ms = String(d.getMilliseconds()).padStart(3, '0');
   timer.textContent = `${m}:${s}.${ms}`;
-  setTimeout(() => {
+
+  timeoutId = setTimeout(() => {
     countUp();
   }, 10);
 }
@@ -23,4 +26,15 @@ start.addEventListener('click', () => {
   startTime = Date.now();
   countUp();
 });
+
+stop.addEventListener('click', () => {
+  clearTimeout(timeoutId);
+  elapsedTime += Date.now() - startTime;
+});
+
+reset.addEventListener('click', () => {
+  timer.textContent = '00:00.000';
+  elapsedTime = 0;
+});
+
 }
